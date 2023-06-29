28b8b2d871d2571b96496bf97e20b11a567e5efb
ZhurkinN <zhurkin236@gmail.com>
2023-06-29 22:59:48 +0300

    add: liquibase changelog files

--- src/main/resources/db/init/create.sql
+++ src/main/resources/db/init/create.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS genre;
 
 CREATE TABLE tbl_users (
-                       id   BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+                       id   BIGINT NOT NULL UNIQUE PRIMARY KEY,
                        name  VARCHAR(255) NOT NULL UNIQUE
 );
