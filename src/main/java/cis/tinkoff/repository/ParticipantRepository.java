998f0cb498da46427d6615b74521bf1962088308
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-07-01 13:51:26 +0300

    feat: add pageable participant where user is null

--- src/main/java/cis/tinkoff/repository/ParticipantRepository.java
+++ src/main/java/cis/tinkoff/repository/ParticipantRepository.java
@@ -2,9 +2,13 @@ package cis.tinkoff.repository;
 
 import cis.tinkoff.model.Participant;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.Pageable;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
+import java.util.List;
+
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface ParticipantRepository extends CrudRepository<Participant, Long> {
+    List<Participant> findByUserIsNull(Pageable pageable);
 }
