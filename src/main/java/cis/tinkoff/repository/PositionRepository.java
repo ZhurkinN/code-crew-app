43c5f31d7c31193870dd405a1ba43ed0ff8b7e9c
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-07-02 13:46:56 +0300

    feat: add query specification to some methods

--- src/main/java/cis/tinkoff/repository/PositionRepository.java
+++ src/main/java/cis/tinkoff/repository/PositionRepository.java
@@ -1,14 +1,17 @@
 package cis.tinkoff.repository;
 
 import cis.tinkoff.model.Position;
+import io.micronaut.data.annotation.Where;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.repository.jpa.criteria.QuerySpecification;
 
 import java.util.List;
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface PositionRepository extends CrudRepository<Position, Long> {
-    List<Position> findByUserIsNull(Pageable pageable);
+    Page<Position> findAll(QuerySpecification<Position> spec, Pageable pageable);
 }
--- src/main/java/cis/tinkoff/repository/PositionRequestRepository.java
+++ src/main/java/cis/tinkoff/repository/PositionRequestRepository.java
@@ -1,10 +1,21 @@
 package cis.tinkoff.repository;
 
+import cis.tinkoff.model.Position;
 import cis.tinkoff.model.PositionRequest;
+import io.micronaut.data.annotation.Join;
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.repository.jpa.criteria.QuerySpecification;
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface PositionRequestRepository extends CrudRepository<PositionRequest, Long> {
+    Page<PositionRequest> findAll(QuerySpecification<Position> spec, Pageable pageable);
+
+    @Join(value = "resume", type = Join.Type.FETCH)
+    @Query(value = "select * from position_request ps join (resume rm join users us on rm.user_id = us.id) on ps.resume_id = rm.id")
+    Page<PositionRequest> findAll(Pageable pageable);
 }
