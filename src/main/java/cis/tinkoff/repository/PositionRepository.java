6d525c1b93eacc0145a2bd3f002183062d657483
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-07-04 00:02:11 +0300

    fix: try to fix repositories

--- src/main/java/cis/tinkoff/repository/PositionRepository.java
+++ src/main/java/cis/tinkoff/repository/PositionRepository.java
@@ -1,18 +1,16 @@
 package cis.tinkoff.repository;
 
 import cis.tinkoff.model.Position;
-import io.micronaut.data.annotation.Where;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
 import io.micronaut.data.model.query.builder.sql.Dialect;
-import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.repository.PageableRepository;
 import io.micronaut.data.repository.jpa.JpaSpecificationExecutor;
-import io.micronaut.data.repository.jpa.criteria.QuerySpecification;
-
-import java.util.List;
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
-public interface PositionRepository extends CrudRepository<Position, Long>, JpaSpecificationExecutor<Position> {
-    Page<Position> findAll(QuerySpecification<Position> spec, Pageable pageable);
+public interface PositionRepository extends PageableRepository<Position, Long>, JpaSpecificationExecutor<Position> {
+//    @Query(value = "SELECT position_ FROM positions position_ ", countQuery = "SELECT count(position_) FROM positions position_ ")
+//    @Join(value = "customer", type = Join.Type.FETCH)
+    Page<Position> findAll(Pageable pageable);
 }
--- src/main/java/cis/tinkoff/support/helper/Specifications.java
+++ src/main/java/cis/tinkoff/support/helper/Specifications.java
@@ -3,16 +3,14 @@ package cis.tinkoff.support.helper;
 import cis.tinkoff.model.Position;
 import io.micronaut.data.repository.jpa.criteria.QuerySpecification;
 
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.Locale;
 
 public class Specifications {
     public static QuerySpecification<Position> searchVacancies(Long date, String status, String direction, String skills) {
         return (root, query, criteriaBuilder) -> {
             query.select(root.get("id"))
                     .where(
-                            criteriaBuilder.lessThan(root.get("createdWhen"), new Timestamp(date))
+                            criteriaBuilder.greaterThan(root.get("createdWhen"), new Timestamp(date))
                     )
 //                    .where(
 //                            criteriaBuilder.equal(root.get("direction"), direction)
