aa92c8e550492b8db1f46144598c46ecdf57237a
Alexey Timofeev <lesha.timofeev2017@yandex.ru>
2023-07-11 23:18:28 +0300

    fix: h2 console enable

--- src/main/java/cis/tinkoff/Application.java
+++ src/main/java/cis/tinkoff/Application.java
@@ -8,6 +8,9 @@ import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -30,7 +33,8 @@ public class Application {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
         Micronaut.run(Application.class, args);
     }
 }
