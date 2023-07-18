ffa8095794e75f5ed6ff0d5c8a1c4899f25080e5
ZhurkinN <zhurkin236@gmail.com>
2023-07-18 07:11:22 +0300

    add: gradle build in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
+FROM gradle:7.6.1-jdk17-alpine AS build
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle build --no-daemon
+
 FROM openjdk:17
 
 EXPOSE 8080
-COPY build/libs/code-crew-0.1-all.jar /app/code-crew.jar
+
+RUN mkdir /app
+
+COPY --from=build /home/gradle/src/build/libs/code-crew-0.1-all.jar /app/code-crew.jar
 COPY /src /app/src
-WORKDIR /app
-ENTRYPOINT ["java", "-jar", "code-crew.jar"]
+
+ENTRYPOINT ["java", "-XX:+UnlockExperimentalVMOptions", "-Djava.security.egd=file:/dev/./urandom","-jar","/app/code-crew.jar"]
--- src/main/java/cis/tinkoff/support/exceptions/constants/ErrorDisplayMessageKeeper.java
+++ src/main/java/cis/tinkoff/support/exceptions/constants/ErrorDisplayMessageKeeper.java
@@ -6,15 +6,12 @@ public interface ErrorDisplayMessageKeeper {
     String RECORD_NOT_FOUND = "The record for this query was not found.";
     String USER_NOT_FOUND = "User was not found.";
     String RESUME_NOT_FOUND = "Resume was not found.";
-    String REQUEST_NOT_FOUND = "Position request was not found.";
     String POSITION_NOT_FOUND = "Position was not found.";
     String DIRECTION_NOT_FOUND = "Resume was not found.";
     String USER_ALREADY_EXISTS = "A user with this email is already registered.";
-    String DELETED_RECORD_FOUND = "Received data has been deleted or is unavailable.";
 
     //Access errors
     String RESUME_WRONG_ACCESS = "Action with this resume is not available.";
-    String DELETED_OR_HIDDEN_RESUME_FOUND = "This resume has been deleted or hidden.";
     String USER_ALREADY_IN_PROJECT = "A user with this email is already in this project.";
     String SAME_REQUEST_ALREADY_EXISTS = "User already created same request. It's still in consideration.";
     String PROJECT_WRONG_ACCESS = "Action with this project is not available.";
@@ -23,5 +20,5 @@ public interface ErrorDisplayMessageKeeper {
     //Exception messages
     String PICTURE_NOT_FOUND = "Profile picture for user with id = %d wasn't found.";
     String WRONG_MEDIA_TYPE = "User with email - %s tried to upload a file with unavailable type. File name - %s";
-    String WRONG_FILE_SIZE = "User with email - %s tried to upload a file too large or empty. The size must be less than %d B.";
+
 }
