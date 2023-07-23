88a351868a436c0c9fe127f781fe51440ac01ae8
EugeneBUSUEK <Eugeny.BUSUEK@gmail.com>
2023-07-23 23:25:33 +0300

    fix: fix null project contacts

--- src/main/java/cis/tinkoff/repository/ProjectRepository.java
+++ src/main/java/cis/tinkoff/repository/ProjectRepository.java
@@ -5,6 +5,7 @@ import cis.tinkoff.model.User;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.Join;
 import io.micronaut.data.annotation.Query;
+import io.micronaut.data.annotation.Where;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
@@ -16,10 +17,11 @@ import java.util.Optional;
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface ProjectRepository extends CrudRepository<Project, Long> {
     @Override
+    @Where("is_deleted = false")
     @Join(value = "leader", type = Join.Type.FETCH)
     @Join(value = "status", type = Join.Type.FETCH)
     @Join(value = "status", type = Join.Type.FETCH)
-    @Join(value = "contacts", type = Join.Type.FETCH)
+    @Join(value = "contacts", type = Join.Type.LEFT_FETCH)
     @Join(value = "positions.direction", type = Join.Type.FETCH)
     @Join(value = "positions.user", type = Join.Type.LEFT_FETCH)
     @Join(value = "members", type = Join.Type.LEFT_FETCH)
